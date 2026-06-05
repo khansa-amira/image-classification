3e8505785831625369f57746469c2f2894100a33
khansa-amira <khnsaamra26@gmail.com>
2026-06-05 21:43:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,21 +2,24 @@ import streamlit as st
 import numpy as np
 from PIL import Image
 import tensorflow as tf
-import gdown
+import requests
 import os
 
 @st.cache_resource
-def load_model_from_drive(file_id):
+def load_model_from_drive():
     output_model_path = 'model_hewan.keras'
     if not os.path.exists(output_model_path):
         with st.spinner('Sedang mengunduh model, mohon tunggu...'):
-            url = f'https://drive.google.com/uc?id={file_id}'
-            gdown.download(url, output_model_path, quiet=False, fuzzy=True)
+            file_id = '1mZO-sDMrG4muobSGu1EknZBFI7uZCr-O'
+            session = requests.Session()
+            url = f'https://drive.google.com/uc?export=download&id={file_id}&confirm=t'
+            response = session.get(url, stream=True)
+            with open(output_model_path, 'wb') as f:
+                for chunk in response.iter_content(32768):
+                    f.write(chunk)
     return tf.keras.models.load_model(output_model_path)
 
-GOOGLE_DRIVE_FILE_ID = '1mZO-sDMrG4muobSGu1EknZBFI7uZCr-O'
-
-model = load_model_from_drive(GOOGLE_DRIVE_FILE_ID)
+model = load_model_from_drive()
 class_names = ['kucing', 'ikan koi']
 
 st.title("Klasifikasi Gambar Hewan")
@@ -27,14 +30,11 @@ uploaded_file = st.file_uploader("Pilih gambar...", type=["jpg", "jpeg", "png"])
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Gambar yang diupload", use_column_width=True)
-    
     img = image.resize((150, 150))
     img_array = np.array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
-    
     prediction = model.predict(img_array)
     predicted_class = class_names[np.argmax(prediction)]
     confidence = np.max(prediction) * 100
-    
     st.success(f"Hasil: **{predicted_class}**")
     st.info(f"Keyakinan: {confidence:.2f}%")
